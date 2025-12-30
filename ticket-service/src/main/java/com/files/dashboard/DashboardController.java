9626b389f68f27dac2d07021b322af9482856a56
Sreenidhi Angadi <angadisreenidhi@gmail.com>
2025-12-30 10:19:06 +0530

    added controller

---
+++ ticket-service/src/main/java/com/files/dashboard/DashboardController.java
@@ -0,0 +1,19 @@
+package com.files.dashboard;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/dashboard")
+@RequiredArgsConstructor
+public class DashboardController {
+
+    private final DashboardService dashboardService;
+
+    @GetMapping("/summary")
+    public Mono<DashboardSummaryResponse> getSummary() {
+        return dashboardService.getSummary();
+    }
+}
