378a236e67c581f17b7299b29fb248917d320d56
Sreenidhi Angadi <angadisreenidhi@gmail.com>
2025-12-31 01:16:49 +0530

    modified service

--- assignment-service/src/main/java/com/files/service/AssignmentService.java
+++ assignment-service/src/main/java/com/files/service/AssignmentService.java
@@ -1,4 +1,5 @@
 package com.files.service;
+import com.files.dto.response.AutoAssignmentResponse;
 import com.files.model.Assignment;
 import reactor.core.publisher.Mono;
 
@@ -7,4 +8,7 @@ public interface AssignmentService {
     Mono<Assignment> assignTicket(String ticketId, String agentId, String priority);
     
     Mono<Assignment> escalate(Assignment assignment, String reason);
+    
+    Mono<AutoAssignmentResponse> autoAssign(String ticketId,String priority);
+
 }
