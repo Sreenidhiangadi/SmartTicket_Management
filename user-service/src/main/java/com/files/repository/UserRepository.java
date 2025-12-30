def8722c7a5bfdb679ee84d717736014e16a7b22
Sreenidhi Angadi <angadisreenidhi@gmail.com>
2025-12-31 01:20:10 +0530

    modified user repository

--- user-service/src/main/java/com/files/repository/UserRepository.java
+++ user-service/src/main/java/com/files/repository/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 import com.files.model.User;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
@@ -11,4 +12,7 @@ public interface UserRepository extends ReactiveMongoRepository<User, String> {
     Mono<User> findByEmail(String email);
 
     Mono<Boolean> existsByEmail(String email);
+    
+    Flux<User> findByRolesContainingAndActive(String role, boolean active);
+
 }
