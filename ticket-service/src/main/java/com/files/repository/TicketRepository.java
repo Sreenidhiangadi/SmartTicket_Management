699ce43b43d5b90bb754c17ec89772a3d97d1d41
Sreenidhi Angadi <angadisreenidhi@gmail.com>
2025-12-30 10:11:10 +0530

    modified ticket repo

--- ticket-service/src/main/java/com/files/repository/TicketRepository.java
+++ ticket-service/src/main/java/com/files/repository/TicketRepository.java
@@ -3,12 +3,23 @@ package com.files.repository;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 import com.files.model.Ticket;
+import com.files.model.TicketPriority;
 import com.files.model.TicketStatus;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface TicketRepository extends ReactiveMongoRepository<Ticket, String> {
 
     Flux<Ticket> findByCreatedBy(String createdBy);
+    
     Flux<Ticket> findByStatus(TicketStatus status);
+    
+    Mono<Long> countByStatus(TicketStatus status);
+
+    Mono<Long> countByPriority(TicketPriority priority);
+
+    Mono<Long> countByAssignedToIsNull();
+    
+    Flux<Ticket> findByStatusIn(Iterable<TicketStatus> statuses);
 }
