124a42e0f515c4ce3299fba9fbbd8b384c7913f6
Sreenidhi Angadi <angadisreenidhi@gmail.com>
2025-12-31 01:28:00 +0530

    added assignmentresponse

---
+++ ticket-service/src/main/java/com/files/dto/AutoAssignmentResponse.java
@@ -0,0 +1,13 @@
+package com.files.dto;
+
+import java.time.Instant;
+
+import lombok.Data;
+
+@Data
+public class AutoAssignmentResponse {
+    private String ticketId;
+    private String agentId;
+    private String priority;
+    private Instant slaDueAt;
+}
