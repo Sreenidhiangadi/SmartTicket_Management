2be83d7962f287de2055c93bf2d12f573e542cf4
Sreenidhi Angadi <angadisreenidhi@gmail.com>
2025-12-31 23:36:19 +0530

    modified listener

--- notification-service/src/main/java/com/files/kafka/TicketAssignedListener.java
+++ notification-service/src/main/java/com/files/kafka/TicketAssignedListener.java
@@ -1,32 +1,41 @@
-//package com.files.kafka;
-//
-//
-//import org.springframework.kafka.annotation.KafkaListener;
-//import org.springframework.stereotype.Component;
-//
-//import com.files.messaging.TicketAssignedEvent;
-//import com.files.model.NotificationType;
-//import com.files.service.NotificationService;
-//
-//import lombok.RequiredArgsConstructor;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class TicketAssignedListener {
-//
-//    private final NotificationService notificationService;
-//
-//    @KafkaListener(
-//            topics = "ticket-assigned-events",
-//            groupId = "notification-service",
-//            containerFactory = "kafkaListenerContainerFactory"
-//    )
-//    public void onMessage(TicketAssignedEvent event) {
-//        notificationService.notifyUser(
-//                event.getAgentId(),
-//                NotificationType.TICKET_ASSIGNED,
-//                "New ticket assigned",
-//                "Ticket " + event.getTicketId() + " has been assigned to you"
-//        ).subscribe();
-//    }
-//}
+package com.files.kafka;
+
+import com.files.messaging.TicketAssignedEvent;
+import com.files.model.NotificationType;
+import com.files.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class TicketAssignedListener {
+
+    private final NotificationService notificationService;
+
+    @KafkaListener(
+        topics = "ticket-assigned-events",
+        groupId = "notification-service",
+        containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void onMessage(TicketAssignedEvent event) {
+
+        String subject = "New Ticket Assigned to You";
+
+        String body =
+            "A new ticket has been assigned to you.\n\n" +
+            "Ticket ID: " + event.getTicketId() + "\n" +
+            "Assigned at: " + event.getAssignedAt();
+
+        notificationService.notifyUser(
+            event.getAgentId(),
+            event.getAgentEmail(),
+            NotificationType.TICKET_ASSIGNED,
+            subject,
+            body
+        ).subscribe();
+    }
+}
+
