fc98b72771cf3ea7bec67bebe742cc94da8d1612
Sreenidhi Angadi <angadisreenidhi@gmail.com>
2026-01-05 19:46:47 +0530

    added tests

--- backend/ticket-service/src/test/java/com/files/TicketServiceApplicationTests.java
+++ backend/ticket-service/src/test/java/com/files/TicketServiceApplicationTests.java
@@ -1,13 +1,23 @@
 package com.files;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
-class TicketServiceApplicationTests {
+class TicketServiceApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // verifies Spring context starts
+    }
 
+    @Test
+    void mainMethodRuns() {
+        assertDoesNotThrow(() ->
+                TicketServiceApplication.main(new String[] {})
+        );
+    }
 }
