d0363de61cfca2af54bc0f4f3269f8ec206ad7aa
Sreenidhi Angadi <angadisreenidhi@gmail.com>
2026-01-06 10:29:17 +0530

    Update home logic

--- frontend/smart-ticket-ui/src/app/features/home/home.ts
+++ frontend/smart-ticket-ui/src/app/features/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { AuthService } from '../../core/auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +10,33 @@ import { RouterModule } from '@angular/router';
   templateUrl: './home.html',
   styleUrls: ['./home.scss']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(
+    public authService: AuthService,
+    private router: Router
+  ) {}
+
+  goToHomeByRole(): void {
+    if (this.authService.hasRole('ADMIN')) {
+      this.router.navigate(['/admin']);
+      return;
+    }
+
+    if (this.authService.hasRole('MANAGER')) {
+      this.router.navigate(['/manager/dashboard']);
+      return;
+    }
+
+    if (this.authService.hasRole('AGENT')) {
+      this.router.navigate(['/agent/queue']);
+      return;
+    }
+
+    if (this.authService.hasRole('USER')) {
+      this.router.navigate(['/tickets/my']);
+      return;
+    }
+
+    this.router.navigate(['/login']);
+  }
+}
